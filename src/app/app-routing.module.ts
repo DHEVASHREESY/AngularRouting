12398551769ccf629c967610cd2abad58bcd59a3
dhevashree <dhevashree18@gmail.com>
2019-08-05 22:34:30 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DepartmentComponent } from './department/department.component';
+import { DepartmentDetailsComponent } from './department-details/department-details.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  
+  {path:'department',component:DepartmentComponent },
+  {path:'department-details',component:DepartmentDetailsComponent},
+  {path:'**',component:ErrorPageComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
